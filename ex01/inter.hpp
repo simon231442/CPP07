11891b692711e81bfeda24fbed45a502b9800d13
Simon Renaud <srenaud@c2r5s6.42lausanne.ch>
2026-04-18 19:37:10 +0200

    asgard :ex01, fct rainbow

--- ex01/inter.hpp
+++ ex01/inter.hpp
@@ -1,11 +1,26 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+# include <iostream>
+
 template <typename T> <typename U>
-void	iter(T* array, int const size, U f)	
+void	iter(T* array, int const size,U f)	
 {
 	for (int i = 0; i < size; i++)
 		f(array[i]);
+}
+
+template <typename T>
+void	printRainbow(T element)
+{
+	static	int		color = 31;
+	static	bool	increase = true;
+
+	std::cout << "\033[1;" << color << "m" << element << "\033[0m";
+
+	increase = (color == 37) ? false : true;
+	increase = (color == 31) ? true : false;
+	(increase) ? color++ : color--;
 }
 
 #endif
---
+++ ex01/main.cpp
@@ -0,0 +1,10 @@
+#include "iter.hpp"
+#include <iostream>
+
+void	printRed(
+
+int		main(void)
+{
+
+	return 0;
+}
